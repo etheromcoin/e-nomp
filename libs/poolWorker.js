58c5d587dfdde528b962e4ed391522bcf041089b
blockinator <36169687+blockinator@users.noreply.github.com>
2022-10-06 08:10:55 -0700

    Update poolWorker.js

--- libs/poolWorker.js
+++ libs/poolWorker.js
@@ -170,6 +170,9 @@ module.exports = function(logger){
         var pool = Stratum.createPool(poolOptions, authorizeFN, logger);
         pool.on('share', function(isValidShare, isValidBlock, data){
 
+            if(data.worker != undefined)
+				data.worker = data.worker.replace(/:/g,"-") 
+
             var shareData = JSON.stringify(data);
 
             if (data.blockHash && !isValidBlock)
